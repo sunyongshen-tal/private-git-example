9edbeb717884b5a11e1fb33b88025c08963c74ab
--local <sunyongshen@tal.com>
2023-05-06 16:59:17 +0800

    feat: 添加流

--- stream.js
+++ stream.js
@@ -12,3 +12,19 @@ server.listen(3000, () => {
     console.log('starting at port 3000');
 }
 );
+
+// 读取一个文件，返回一个流对象
+const fs = require('fs');
+const rs = fs.createReadStream('./test.txt');
+rs.on('data', (chunk) => {
+    console.log(chunk.toString());
+}
+);
+rs.on('end', () => {
+    console.log('END');
+}
+);
+rs.on('error', (err) => {
+
+}
+);
