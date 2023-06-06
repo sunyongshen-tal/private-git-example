11786b5343663544798769b6c61b3173c7c1e171
sunyongshen <sunyongshen@tal.com>
2023-06-06 10:42:53 +0800

    1111

--- stream.js
+++ stream.js
@@ -42,8 +42,4 @@ const ws2 = fs.createWriteStream("./test.txt");
 ws2.write(new Buffer("使用Stream写入二进制数据...\n", "utf-8"));
 ws2.write(new Buffer("END.", "utf-8"));
 
-<<<<<<< HEAD
 // stream 1.0
-=======
-// woshi dev 5.0
->>>>>>> dev
